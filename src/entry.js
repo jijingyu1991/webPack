5f6bd028d9274153b6d169137b3461fbbd8781fc
guantingting <guantingting@menusifu.cn>
2018-02-06 10:15:10 +0800

    打包css
    热更新打包css

---
+++ src/entry.js
@@ -0,0 +1,3 @@
+//入口文件的配置项
+import css from './css/index.css'
+document.getElementById('title').innerHTML='Hello Webpack!!';
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+module.exports={
+  //入口文件的配置项
+  entry:{
+    entry:'./src/entry.js'
+  },
+  //出口文件的配置项
+  output:{
+    //打包的路径文职
+    path:path.resolve(__dirname,'dist'),
+    //打包的文件名称
+    filename:'bundle.js'
+  },
+  //模块：例如解读CSS,图片如何转换，压缩
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use:['style-loader','css-loader']
+      }
+    ]
+  },
+  //插件，用于生产模版和各项功能
+  plugins:[],
+  //配置webpack开发服务功能
+  devServer:{  //webpack3.6开始webpack-dev-server直接支持热更新
+    //设置基本目录结构
+    contentBase:path.resolve(__dirname,'dist'),
+    //服务器的IP地址，可以使用IP也可以使用localhost
+    host: '192.168.0.101',
+    //服务端压缩是否开启
+    compress: true,
+    //配置服务端口
+    port: 1711
+  }
+}
